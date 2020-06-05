01e1e4f5650f5dfe1c4c9b353fa18efe92bb81f7
Matt <mw15macf@alum.siena.edu>
2020-06-05 13:11:33 -0400

    Add MongoDB and Mongoose so posts are saved even after app.js is rerun

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,12 +17,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// working with a local database for now
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+// Initialize the blog post schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// Initialize the blog post model
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeContent: homeStartingContent,
-    postItems: posts
+  Post.find({}, function(err, posts) {
+    res.render("home", {
+      homeContent: homeStartingContent,
+      postItems: posts
+    });
   });
 });
 
@@ -37,26 +50,26 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-    if(storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.body
-      });
-    }
-  });
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId
+  Post.findOne({_id: requestedPostId}, function(err, post){
+   res.render("post", {
+     title: post.title,
+     content: post.content
+   });
+ });
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
-    body: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+    content: req.body.postBody
+  });
+  post.save(function(err) {
+    if(!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function() {
